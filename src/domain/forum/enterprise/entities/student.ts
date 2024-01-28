05f492fad5349c29d424331b5a8e905ff327c47b
Yuji Arima <arimayuji2@gmail.com>
2024-01-28 15:42:48 -0300

    refactor: :recycle: aggregate root entity as parent
    it changes the students hierach by extending a different entity

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -1,18 +1,31 @@
-import { Entity } from '@/core/entities/entity'
-import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { AggregateRoot } from "@/core/entities/aggregate-root";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 
-interface StudentProps {
-  name: string
+export interface StudentProps {
+	name: string;
+	email: string;
+	password: string;
 }
-export class Student extends Entity<StudentProps> {
-  static create(props: StudentProps, id?: UniqueEntityId) {
-    const student = new Student(
-      {
-        ...props,
-      },
-      id,
-    )
+export class Student extends AggregateRoot<StudentProps> {
+	static create(props: StudentProps, id?: UniqueEntityId) {
+		const student = new Student(
+			{
+				...props,
+			},
+			id
+		);
 
-    return student
-  }
+		return student;
+	}
+	get name() {
+		return this.props.name;
+	}
+
+	get email() {
+		return this.props.email;
+	}
+
+	get password() {
+		return this.props.password;
+	}
 }
