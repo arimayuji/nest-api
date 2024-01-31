4f3e29fa2eba5c946dd9cecc725d5cebafeb0e10
Yuji Arima <arimayuji2@gmail.com>
2024-01-31 00:16:27 -0300

    test: :white_check_mark: edit question e2e test

---
+++ src/infra/http/controllers/edit-question.controller.e2e-spec.ts
@@ -0,0 +1,60 @@
+import { AppModule } from "@/infra/app.module";
+import { DatabaseModule } from "@/infra/database/database.module";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { INestApplication } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Test } from "@nestjs/testing";
+import request from "supertest";
+import { StudentFactory } from "test/factories/make-student";
+import { QuestionFactory } from "test/factories/make-question";
+
+describe("Edit Question (E2E)", () => {
+	let app: INestApplication;
+	let prisma: PrismaService;
+	let jwt: JwtService;
+	let studentFactory: StudentFactory;
+	let questionFactory: QuestionFactory;
+
+	beforeAll(async () => {
+		const moduleRef = await Test.createTestingModule({
+			imports: [AppModule, DatabaseModule],
+			providers: [StudentFactory, QuestionFactory],
+		}).compile();
+
+		app = moduleRef.createNestApplication();
+		jwt = moduleRef.get(JwtService);
+		prisma = moduleRef.get(PrismaService);
+		questionFactory = moduleRef.get(QuestionFactory);
+		studentFactory = moduleRef.get(StudentFactory);
+		await app.init();
+	});
+
+	test("[PUT] /questions", async () => {
+		const user = await studentFactory.makePrismaStudent();
+
+		const accessToken = jwt.sign({ sub: user.id.toString() });
+
+		const question = await questionFactory.makePrismaQuestion({
+			authorId: user.id,
+		});
+		const questionId = question.id.toString();
+
+		const response = await request(app.getHttpServer())
+			.put(`/questions/${questionId}`)
+			.set("Authorization", `Bearer ${accessToken}`)
+			.send({
+				title: "New title",
+				content: "New content",
+			});
+
+		expect(response.statusCode).toBe(204);
+
+		const questionOnDatabase = await prisma.question.findFirst({
+			where: {
+				title: "New title",
+			},
+		});
+
+		expect(questionOnDatabase).toBeTruthy();
+	});
+});
