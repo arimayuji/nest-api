30ece11ffcadcbdd13a8bed2d9106a66ac580415
Yuji Arima <arimayuji2@gmail.com>
2024-01-31 00:17:17 -0300

    feat: :sparkles: fetch question answer controller

--- src/domain/forum/application/use-cases/fetch-question-answers.ts
+++ src/domain/forum/application/use-cases/fetch-question-answers.ts
@@ -1,6 +1,7 @@
 import { Either, right } from "@/core/either";
 import { Answer } from "../../enterprise/entities/answer";
 import { AnswersRepository } from "../repositories/answer-repository";
+import { Injectable } from "@nestjs/common";
 
 interface FetchQuestionsAnswersUseCaseRequest {
 	questionId: string;
@@ -13,7 +14,7 @@ type FetchQuestionsAnswersUseCaseResponse = Either<
 		answers: Answer[];
 	}
 >;
-
+@Injectable()
 export class FetchQuestionsAnswersUseCase {
 	constructor(private answersRepository: AnswersRepository) {}
 
