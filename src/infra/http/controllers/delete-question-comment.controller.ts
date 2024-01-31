7eb06f8354a7410cf6a9d2a52ec90a3ec647bb38
Yuji Arima <arimayuji2@gmail.com>
2024-01-31 00:14:11 -0300

    feat: :sparkles: delete question comment controler

---
+++ src/infra/http/controllers/delete-question-comment.controller.ts
@@ -0,0 +1,33 @@
+import {
+	BadRequestException,
+	Controller,
+	Delete,
+	HttpCode,
+	Param,
+} from "@nestjs/common";
+import { TokenPayload } from "@/infra/auth/auth.strategy";
+import { CurrentUser } from "@/infra/auth/current-user-decorator";
+import { DeleteQuestionCommentUseCase } from "@/domain/forum/application/use-cases/delete-question-comment";
+@Controller("/questions/comments/:id")
+export class DeleteQuestionCommentController {
+	constructor(
+		private deleteQuestionCommentUseCase: DeleteQuestionCommentUseCase
+	) {}
+
+	@Delete()
+	@HttpCode(204)
+	async handle(
+		@CurrentUser()
+		user: TokenPayload,
+		@Param("id") questionCommentId: string
+	) {
+		const userId = user.sub;
+		const result = await this.deleteQuestionCommentUseCase.execute({
+			authorId: userId,
+			questionCommentId,
+		});
+		if (result.isLeft()) {
+			throw new BadRequestException();
+		}
+	}
+}
