32ef165493e0ab88dc298c8499de046841cc47db
Yuji Arima <arimayuji2@gmail.com>
2024-02-07 15:15:28 -0300

    test: :white_check_mark: unity test to verify the attachments persist

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -40,4 +40,26 @@ describe("Create Question", () => {
 			expect.objectContaining({ attachmentId: new UniqueEntityId("2") }),
 		]);
 	});
+
+	it("should persist attachments when creating a new question", async () => {
+		const result = await sut.execute({
+			authorId: "1",
+			content: "Hello World",
+			title: "My first text",
+			attachmentsIds: ["1", "2"],
+		});
+
+		expect(result.isRight()).toBe(true);
+		expect(inMemoryQuestionsAttachmentsRepository.items).toHaveLength(2);
+		expect(inMemoryQuestionsAttachmentsRepository.items).toEqual(
+			expect.arrayContaining([
+				expect.objectContaining({
+					attachmentId: new UniqueEntityId("1"),
+				}),
+				expect.objectContaining({
+					attachmentId: new UniqueEntityId("2"),
+				}),
+			])
+		);
+	});
 });
