3d4e04ffd89759757d20751b5332dc58ae0a1684
Yuji Arima <arimayuji2@gmail.com>
2024-02-09 08:49:26 -0300

    feat: :sparkles: add fetch question comment use-case

---
+++ src/domain/forum/application/use-cases/fetch-question-comments.ts
@@ -0,0 +1,32 @@
+import { Either, right } from "@/core/either";
+import { QuestionCommentsRepository } from "../repositories/question-comments-repository";
+import { Injectable } from "@nestjs/common";
+import { CommentWithAuthor } from "../../enterprise/entities/value-objects/comment-with-author";
+
+interface FetchQuestionCommentsUseCaseRequest {
+	questionId: string;
+	page: number;
+}
+
+type FetchQuestionCommentsUseCaseResponse = Either<
+	null,
+	{
+		comments: CommentWithAuthor[];
+	}
+>;
+@Injectable()
+export class FetchQuestionCommentsUseCase {
+	constructor(private answersRepository: QuestionCommentsRepository) {}
+
+	async execute({
+		questionId,
+		page,
+	}: FetchQuestionCommentsUseCaseRequest): Promise<FetchQuestionCommentsUseCaseResponse> {
+		const comments =
+			await this.answersRepository.findManyByQuestionIdWithAuthor(questionId, {
+				page,
+			});
+
+		return right({ comments });
+	}
+}
