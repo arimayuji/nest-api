77c6d8d00825b153c7eb1cd3e423975f0fc8dcf9
Yuji Arima <arimayuji2@gmail.com>
2024-02-09 08:58:13 -0300

    feat: :sparkles: add findManyByQuestionIdWithAuthor implementation

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,11 +1,15 @@
 import { PaginationParams } from "@/core/repositories/pagination-params";
 import { QuestionCommentsRepository } from "@/domain/forum/application/repositories/question-comments-repository";
 import { QuestionComment } from "@/domain/forum/enterprise/entities/question-comment";
+import { InMemoryStudentsRepository } from "./in-memory-students-repository";
+import { CommentWithAuthor } from "@/domain/forum/enterprise/entities/value-objects/comment-with-author";
 
 export class InMemoryQuestionCommentsRepository
 	implements QuestionCommentsRepository
 {
 	public items: QuestionComment[] = [];
+
+	constructor(private inMemoryStudentsRepository: InMemoryStudentsRepository) {}
 
 	async create(questionComment: QuestionComment) {
 		this.items.push(questionComment);
@@ -18,7 +22,7 @@ export class InMemoryQuestionCommentsRepository
 
 		this.items.splice(itemIndex, 1);
 	}
-	
+
 	async findById(id: string) {
 		const questionComment = this.items.find(
 			(item) => item.id.toString() === id
@@ -39,5 +43,38 @@ export class InMemoryQuestionCommentsRepository
 			.slice((page - 1) * resultsPerPage, page * resultsPerPage);
 
 		return questionComments;
+	}
+
+	async findManyByQuestionIdWithAuthor(
+		questionId: string,
+		{ page }: PaginationParams
+	) {
+		const resultsPerPage = 20;
+		
+		const questionCommentsWithAuthor = this.items
+			.filter((item) => item.questionId.toString() === questionId)
+			.slice((page - 1) * resultsPerPage, page * resultsPerPage)
+			.map((comment) => {
+				const author = this.inMemoryStudentsRepository.items.find((student) => {
+					return student.id.equals(comment.authorId);
+				});
+
+				if (!author) {
+					throw new Error(
+						`Author with ID "${comment.authorId.toString()}" does not exist`
+					);
+				}
+
+				return CommentWithAuthor.create({
+					content: comment.content,
+					commentId: comment.id,
+					authorId: comment.authorId,
+					createdAt: comment.createdAt,
+					updatedAt: comment.updatedAt,
+					author: author.name,
+				});
+			});
+
+		return questionCommentsWithAuthor;
 	}
 }
